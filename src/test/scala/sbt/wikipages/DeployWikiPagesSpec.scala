42d1a7d9d112f7663ad84b06897fbf587ccd1453
Pablo Lalloni <plalloni@gmail.com>
2013-03-17 23:27:24 -0300

    Add test code

---
+++ src/test/scala/sbt/wikipages/DeployWikiPagesSpec.scala
@@ -0,0 +1,24 @@
+package sbt.wikipages
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSpec
+import sbt._
+import org.json4s.jackson.JsonMethods._
+
+class DeployWikiPagesSpec extends FunSpec with ShouldMatchers {
+
+  describe("deploy-wiki-pages") {
+
+    for (
+      (f, n, r) ← WikiPages.WikiPagesTasks.deployWikiPages(
+        file("src/test/wiki"),
+        "my/test/1.0",
+        "http://issuetrackerddit.afip.gov.ar/pruebas/login/jsonrpc",
+        Seq(Credentials("Trac", "issuetrackerddit.afip.gov.ar", "pruebas", "pruebas")))
+    ) {
+      println("%s %s: %s" format (f, n, compact(render(r))))
+    }
+
+  }
+
+}
